ef2a8e379132ccbf89a9d7e3f5998bcb27b3009d
Kan215 <katamialvin215@gmail.com>
2024-02-21 00:30:04 +0300

    simple_shell

--- simple_shell.c
+++ simple_shell.c
@@ -6,89 +6,46 @@
 
 #define MAX_COMMAND_LENGTH 2048
 
-void display_prompt()
+void display_promt()
 {
-	printf(" # ");
+	printf(" $ ");
 }
 
-int execute_command(char *command)
+int execute_shell_command(char *command)
 {
-	if (strcmp(command, "exit") == 0)
+	pid_t pid = fork();
+
+	if (pid < 0)
 	{
-	printf("Exiting Shell.. Please Wait..\n");
-	exit(EXIT_SUCCESS);
+		perror("fork");
+		return (-1);
 	}
-	else if (strncmp(command, "cd ", 3) == 0)
+	else if (pid == 0)
 	{
-		char *dir = command + 3;
-
-	if (chdir(dir) != 0)
-	{
-	perror("chdir");
-	return (-1);
-	}
-	return (0);
-	}
-
-
-	else if (strcmp(command, "cd") == 0)
-	{
-		char *home_dir = getenv("HOME");
-
-		if (home_dir == NULL)
-		{
-			fprintf(stderr, "Home Environment Not set\n");
-			return (-1);
-		}
-		if (chdir(home_dir) != 0)
-		{
-			perror("chdir");
-			return (-1);
-		}
-		return (0);
-	}
-	else if (strncmp(command, "emacs ", 6) == 0)
-	{
-		char *args[] = { "emacs", &command[6], NULL };
-
-		execvp("emacs", args);
+		execlp(command, command, NULL);
+		fprintf(stderr, "Error: Command '%s' not found\n", command);
+		exit(EXIT_FAILURE);
 	}
 	else
 	{
-		pid_t pid = fork();
-
-		if (pid < 0)
-		{
-			perror("fork");
-			return (-1);
-		}
-		else if (pid == 0)
-		{
-			execlp(command, command, NULL);
-			fprintf(stderr, "Error: Command '%s' not found\n", command);
-			exit(EXIT_FAILURE);
-		}
-		else
-		{
-			int status;
-			waitpid(pid, &status, 0);
-		}
+		int status;
+		waitpid(pid, &status, 0);
 	}
 	return (0);
 }
 
-int main(void)
+int main (void)
 {
 	char command[MAX_COMMAND_LENGTH];
 
 	while (1)
 	{
-		display_prompt();
-		if  (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
+		display_promt();
+		if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
 		{
 			if (feof(stdin))
 			{
-				printf("\n Exiting Shell ...\n");
+				printf("\nExiting shell...\n");
 				break;
 			}
 			else
@@ -98,10 +55,12 @@ int main(void)
 			}
 		}
 		command[strcspn(command, "\n")] = '\0';
-		if (execute_command(command) < 0)
+
+		if (execute_shell_command(command) < 0)
 		{
 			return (EXIT_FAILURE);
 		}
 	}
+
 	return (EXIT_SUCCESS);
 }
