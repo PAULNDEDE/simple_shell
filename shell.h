6b81a00d8dd755c4bbf745fb6eaa45ebf8d7dc50
PAULNDEDE <paulodhiambo193@gmail.com>
2024-02-25 07:16:39 -0800

    simple_shell

---
+++ shell.h
@@ -0,0 +1,27 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/* header files */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+
+
+
+
+
+
+
+
+
+#endif
