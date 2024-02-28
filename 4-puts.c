3f6cae07f8a69532a3d167928e50192388984261
Kan215 <katamialvin215@gmail.com>
2024-02-28 16:52:23 +0300

    0. Betty would be proud

---
+++ 4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry of code
+ * prints "programming is like building a multiligual puzle
+ * * followed by new line"
+ */
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+
+	return (0);
+}
