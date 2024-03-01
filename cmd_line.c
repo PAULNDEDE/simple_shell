41b3eb2eff138e19b6d5700e7e4a05d07f80c3b7
Kan215 <katamialvin215@gmail.com>
2024-03-01 12:48:05 +0300

    first files

--- cmd_line.c
+++ cmd_line.c
@@ -1,62 +1,25 @@
 #include "main.h"
 
-void shell(void)
-{
-	char input[100];
-	char *command;
-	char *args[10];
+#define MAX_ARGS 10
 
-	while (1)
-	{
-	printf("$ ");
-	fflush(stdout);
+void parse_input(char *input, char **command, char **args) {
+    char *token;
+    int i;
+    int arg_index = 0;
 
-	if (fgets(input, sizeof(input), stdin) == NULL)
-	{
-	printf("\n");
-	break;
-	}
+    /* Initialize args array */
+    for (i = 0; i < MAX_ARGS; i++) {
+        args[i] = NULL;
+    }
 
-	/*get user input*/
-	command = strtok(input, " \n");
-	int a = 0;
-
-	while (command != NULL && a < 10)
-	{
-	args[a] = command;
-	command = strtok(NULL, " \n");
-	a++;
-}
-	args[a] = NULL;
-	pid_t pid = fork();
-
-	if (pid < 0)
-	{
-	perror("Fork failed");
-	exit(EXIT_FAILURE);
-	}
-	else if (pid == 0)
-	{
-	execvp(args[0], args);
-	printf("Err: Command not found: %s\n", args[0]);
-	exit(EXIT_FAILURE);
-	}
-	else
-{
-	waitpid(pid, NULL, 0);
-	}
-	}
-}
-
-/**
- * main: Entry of thee program
- *
- * Return:Always  0(success)
- *
- */
-
-int main(void)
-{
-	shell();
-	return (0);
+    token = strtok(input, " \n");
+    if (token != NULL) {
+        *command = token;
+        while ((token = strtok(NULL, " \n")) != NULL && arg_index < MAX_ARGS - 1) {
+            args[arg_index++] = token;
+        }
+        args[arg_index] = NULL;
+    } else {
+        *command = NULL;
+    }
 }
--- shell.c
+++ shell.c
@@ -1,19 +1,20 @@
-#include "main.h"
+"#include "main.h"
 
-void env_command(void); // Declare env_command before usage
+
+void env_command(void);
 
 void parse_input(char *input, char **command, char **args) {
-    int a; // Move variable declaration to the beginning of the block
+    char *token;
+    int i;
 
-    // Initialize args array
-    for (a = 0; a < 10; a++) {
-        args[a] = NULL;
+    for (i = 0; i < 10; i++) {
+        args[i] = NULL;
     }
 
-    char *token = strtok(input, " \n");
+    token = strtok(input, " \n");
     if (token != NULL) {
         *command = token;
-        int i = 0;
+        i = 0;
         while ((token = strtok(NULL, " \n")) != NULL && i < 9) {
             args[i] = token;
             i++;
@@ -27,43 +28,53 @@ void shell(void) {
     char input[100];
     char *command;
     char *args[10];
+    pid_t pid;
 
-    while (1) {
-        // Display prompt
+    while (1)
+    {
         printf("$ ");
         fflush(stdout);
 
-        // Read input from user
+        /* Read input from user*/
         if (fgets(input, sizeof(input), stdin) == NULL) {
-            // Handle EOF
+            /* Handle EOf*/
             printf("\n");
             break;
         }
 
-        // Parse input
+        /* Parse input*/
         parse_input(input, &command, args);
 
-        // Execute built-in commands
+        /* Execute builtin commands*/
         if (command != NULL && strcmp(command, "env") == 0) {
-            env_command(); // Call env_command if the input is "env"
+            env_command();
         } else {
-            // Execute external commands
-void execute_command(char *command,char **args);
+            /* Execute external commands*/
+            pid = fork();
+            if (pid == -1) {
+                perror("fork");
+            } else if (pid == 0) {
+                execve(command, args, NULL);
+                perror("execve");
+                exit(EXIT_FAILURE);
+            } else {
+                wait(NULL);
+            }
         }
     }
 }
 
 int main(void) {
-    // Start the shell
+  
     shell();
     return 0;
 }
 
 void env_command(void) {
-    extern char **environ; // External variable containing the environment variables
+    extern char **environ;
+    int a;
 
-    // Print each environment variable
-    for (int a = 0; environ[a] != NULL; a++) {
+    for (a = 0; environ[a] != NULL; a++) {
         printf("%s\n", environ[a]);
     }
 }
