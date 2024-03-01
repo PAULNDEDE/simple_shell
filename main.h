be9b60590e74b99650a2a1395f7cff9c0019a930
Kan215 <katamialvin215@gmail.com>
2024-03-01 13:19:44 +0300

    first files

--- main.h
+++ main.h
@@ -2,27 +2,18 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-
-void execute_command(char *command, char **args);
-/*void env_command(void);*/
+/* Function prototypes*/
 void parse_input(char *input, char **command, char **args);
-
-
-/**
- * shell - A simple shell interpreter
- */
 void shell(void);
-
-
-
-
-
+void env_command(void);
+void exit_command(void);
 
 
 #endif /* MAIN_H */
-
--- shell.c
+++ shell.c
@@ -1,28 +1,13 @@
-"#include "main.h"
+#include "main.h"
+
+void exit_command(void)
+{
+	exit(EXIT_SUCCESS);
+}
+
 
 
 void env_command(void);
-
-void parse_input(char *input, char **command, char **args) {
-    char *token;
-    int i;
-
-    for (i = 0; i < 10; i++) {
-        args[i] = NULL;
-    }
-
-    token = strtok(input, " \n");
-    if (token != NULL) {
-        *command = token;
-        i = 0;
-        while ((token = strtok(NULL, " \n")) != NULL && i < 9) {
-            args[i] = token;
-            i++;
-        }
-    } else {
-        *command = NULL;
-    }
-}
 
 void shell(void) {
     char input[100];
@@ -30,26 +15,25 @@ void shell(void) {
     char *args[10];
     pid_t pid;
 
-    while (1)
-    {
+    while (1) {
         printf("$ ");
         fflush(stdout);
 
-        /* Read input from user*/
+        
         if (fgets(input, sizeof(input), stdin) == NULL) {
-            /* Handle EOf*/
+           
             printf("\n");
             break;
         }
 
-        /* Parse input*/
+        
         parse_input(input, &command, args);
 
-        /* Execute builtin commands*/
+      
         if (command != NULL && strcmp(command, "env") == 0) {
             env_command();
         } else {
-            /* Execute external commands*/
+           
             pid = fork();
             if (pid == -1) {
                 perror("fork");
@@ -65,7 +49,6 @@ void shell(void) {
 }
 
 int main(void) {
-  
     shell();
     return 0;
 }
