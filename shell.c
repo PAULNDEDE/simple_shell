3e81ec451f169d4b68740fb4c5504c1e2e89d2db
Kan215 <katamialvin215@gmail.com>
2024-03-02 04:19:49 +0300

    simple_shell

--- shell.c
+++ shell.c
@@ -1,45 +1,48 @@
 #include "main.h"
 
 /**
- * main -the entry point of the program
- * @argc: arguments count
- * @argv: an array of arguments
- * Return:ALways 0(success)
- *
+ * main - Entry point of the Program
+ * @argc: Arguments
+ * @argv: Array of Arguments
+ * Return: 0 (success)
  */
 int main(int argc, char **argv)
 {
-	int command_count = 0;
-	ssize_t line_length = 0;
-	size_t buffer_size = 0;
-	char *input_buffer = NULL;
+    int counter = 0;
+    ssize_t index = 0;
+    size_t size = 0;
+    char *buffer = NULL;
 
-	if (argc < 0)
-	exit(EXIT_FAILURE);
-	while (1)
-	{
-	command_count++;
-	input_buffer = NULL;
-	handle_signl(SIGINT);
-	if (isatty(STDIN_FILENO))
-	write(STDOUT_FILENO, "$ ", 2);
-	line_length = getline(&input_buffer, &buffer_size, stdin);
-	if (line_length == -1)
-	{
-	free(input_buffer);
-	exit(0);
-	}
-	if (input_buffer[0] != '\n')
-	{
-	if (input_buffer[0] != '#')
-	{
-	input_buffer = hash_handler(input_buffer);
-	tokenizer(input_buffer, argv, command_count);
-	}
-	free(input_buffer);
-	}
-	else
-	free(input_buffer);
-	}
-	return (0);
+    if (argc < 0)
+        exit(EXIT_FAILURE);
+    while (1)
+    {
+        counter++;
+        buffer = NULL;
+        signal(SIGINT, handle_signl);
+        
+        
+        if (isatty(STDIN_FILENO))
+            write(STDOUT_FILENO, "$ ", 2);
+        
+        index = getline(&buffer, &size, stdin);
+        if (index == -1)
+        {
+            free(buffer);
+            exit(0);
+        }
+        if (buffer[0] != '\n')
+        {
+            if (buffer[0] != '#')
+            {
+                buffer = hash_handler(buffer);
+                tokenizer(buffer, argv, counter);
+            }
+            free(buffer);
+        }
+        else
+            free(buffer);
+    }
+    return (0);
 }
+
