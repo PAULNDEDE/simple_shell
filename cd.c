9b446a13b1e22a89394200fa2b63d8de9871aaba
Kan215 <katamialvin215@gmail.com>
2024-03-06 15:38:53 +0300

    cd

--- cd.c
+++ cd.c
@@ -1,32 +1,49 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 /**
- * cd - changes directory
- * @tokens: tokenized user input
- * Return: 0 on suucees
- * -1 on err an errno set appropriately
+ * cd - Function changes directory
+ * @args: user input arguments
+ * Return: Always 0 (success), else -1 on error
  */
-int cd(char **tokens)
-{
-	char *home = getenv("HOME");
-	int return_value = 0;
+int cd(char **tokens) {
+    char *home_dir = getenv("HOME");
+    char *prev_dir = getenv("PWD");
+    int return_value = 0;
 
-	if (strcmp(tokens[1], ".") == 0)
-		;
-	else if (strcmp(tokens[1], "~") == 0 || (tokens[1] == NULL))
-	{
-		return_value = chdir(home);
-		if (return_value == -1)
-		{
-			perror("cd");
-			return (-1);
-		}
-	}
-	else
-	{
-		return_value = chdir(tokens[1]);
-		if (return_value == -1)
-			return (-1);
-	}
-	return (return_value);
+    
+    char cwd[1024];
+
+    if (!tokens[1] || strcmp(tokens[1], "~") == 0) {
+        return_value = chdir(home_dir);
+    } else if (strcmp(tokens[1], "-") == 0) {
+        if (!prev_dir) {
+            fprintf(stderr, "cd: OLDPWD not set\n");
+            return -1;
+        }
+        return_value = chdir(prev_dir);
+    } else {
+        return_value = chdir(tokens[1]);
+    }
+
+    if (return_value == -1) {
+        perror("cd");
+        return -1;
+    }
+
+    
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        setenv("PWD", cwd, 1);
+    } else {
+        perror("getcwd() error");
+        return -1;
+    }
+
+    /* Update OLDPWD environment variable*/
+    setenv("OLDPWD", prev_dir ? prev_dir : "", 1);
+
+    return 0;
 }
-
