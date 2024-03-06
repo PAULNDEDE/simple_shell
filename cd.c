6151e2b4722e3b7d245af82ac021e5a3474dfbd9
Kan215 <katamialvin215@gmail.com>
2024-03-06 16:32:08 +0300

    cd

--- cd.c
+++ cd.c
@@ -10,31 +10,35 @@
  * Return: Always 0 (success), else -1 on error
  */
 int cd(char **tokens) {
+    char cwd[1024];
     char *home_dir = getenv("HOME");
     char *prev_dir = getenv("PWD");
-    char cwd[1024];
     char *new_dir;
     int return_value = 0;
 
+   
     if (tokens[1] == NULL || strcmp(tokens[1], "~") == 0) {
         new_dir = home_dir;
     } else if (strcmp(tokens[1], "-") == 0) {
+      
         if (prev_dir == NULL) {
             fprintf(stderr, "cd: OLDPWD not set\n");
             return -1;
         }
         new_dir = prev_dir;
     } else {
-
+      
         new_dir = tokens[1];
     }
 
+   
     return_value = chdir(new_dir);
     if (return_value == -1) {
         perror("cd");
         return -1;
     }
 
+    
     if (getcwd(cwd, sizeof(cwd)) == NULL) {
         perror("getcwd");
         return -1;
