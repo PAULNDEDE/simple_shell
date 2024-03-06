854a499c2acac6743e0b367605ea2cc8f49db88d
Kan215 <katamialvin215@gmail.com>
2024-03-06 16:16:37 +0300

    cd

--- cd.c
+++ cd.c
@@ -1,56 +1,52 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 
 /**
  * cd - Function changes directory
- * @args: user input arguments
+ * @tokens: user input arguments
  * Return: Always 0 (success), else -1 on error
  */
-	int cd(char **tokens)
-	{
-	char *home_dir = getenv("HOME");
-	char *prev_dir = getenv("PWD");
-	int return_value = 0;
+int cd(char **tokens) {
+    char *home_dir = getenv("HOME");
+    char *prev_dir = getenv("PWD");
+    char cwd[1024];
+    char *new_dir;
+    int return_value = 0;
 
-	char cwd[1024];
+    if (tokens[1] == NULL || strcmp(tokens[1], "~") == 0) {
+        new_dir = home_dir;
+    } else if (strcmp(tokens[1], "-") == 0) {
+        if (prev_dir == NULL) {
+            fprintf(stderr, "cd: OLDPWD not set\n");
+            return -1;
+        }
+        new_dir = prev_dir;
+    } else {
 
-	if (!tokens[1] || strcmp(tokens[1], "~") == 0)
-	{
-	(return_value = chdir(home_dir));
-	}
-	else if (strcmp(tokens[1], "-") == 0)
-	{
-	if (!prev_dir)
-	{
-	fprintf(stderr, "cd: OLDPWD not set\n");
-	return (-1);
-	}
-	(return_value = chdir(prev_dir));
-	}
-	else
-	{
-	(return_value = chdir(tokens[1]));
-	}
+        new_dir = tokens[1];
+    }
 
-	if (return_value == -1)
-	{
-	perror("cd");
-	return (-1);
-	}
+    return_value = chdir(new_dir);
+    if (return_value == -1) {
+        perror("cd");
+        return -1;
+    }
 
+    if (getcwd(cwd, sizeof(cwd)) == NULL) {
+        perror("getcwd");
+        return -1;
+    }
+    if (setenv("PWD", cwd, 1) == -1) {
+        perror("setenv");
+        return -1;
+    }
+    if (setenv("OLDPWD", prev_dir != NULL ? prev_dir : "", 1) == -1) {
+        perror("setenv");
+        return -1;
+    }
 
-	if (getcwd(cwd, sizeof(cwd)) != NULL)
-	{
-	setenv("PWD", cwd, 1);
-	}
-	else
-	{
-	perror("getcwd() error");
-	return (-1);
-	}
-	setenv("OLDPWD", prev_dir ? prev_dir : "", 1);
-	return (0);
+    return 0;
 }
