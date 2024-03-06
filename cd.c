84e20fbc264ba0b16f3c5b67018e47508c6c8d83
Kan215 <katamialvin215@gmail.com>
2024-03-06 16:59:26 +0300

    cd

--- cd.c
+++ cd.c
@@ -1,8 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
 
 int cd(char **tokens) {
     char cwd[PATH_MAX];
@@ -10,19 +6,23 @@ int cd(char **tokens) {
     char *prev_dir = getenv("PWD");
     char *new_dir;
     int return_value = 0;
-
+    
     char *last_slash;
-    size_t cwd_len;
 
-    if (tokens[1] == NULL || strcmp(tokens[1], "~") == 0) {
+    
+    if (tokens[1] == NULL) {
         new_dir = home_dir;
-    } else if (strcmp(tokens[1], "-") == 0) {
+    }
+   
+    else if (strcmp(tokens[1], "-") == 0) {
         if (prev_dir == NULL) {
             fprintf(stderr, "cd: OLDPWD not set\n");
             return -1;
         }
         new_dir = prev_dir;
-    } else if (strcmp(tokens[1], "..") == 0) {
+    }
+   
+    else if (strcmp(tokens[1], "..") == 0) {
         if (getcwd(cwd, sizeof(cwd)) == NULL) {
             perror("getcwd");
             return -1;
@@ -34,59 +34,30 @@ int cd(char **tokens) {
         }
         *last_slash = '\0';
         new_dir = cwd;
-    } else if (tokens[1][0] == '/') {
+    }
+   
+    else {
         new_dir = tokens[1];
-    } else {
-        if (getcwd(cwd, sizeof(cwd)) == NULL) {
-            perror("getcwd");
-            return -1;
-        }
-        cwd_len = strlen(cwd);
-        new_dir = malloc(cwd_len + strlen(tokens[1]) + 2);
-        if (new_dir == NULL) {
-            perror("malloc");
-            return -1;
-        }
-        strcpy(new_dir, cwd);
-        strcat(new_dir, "/");
-        strcat(new_dir, tokens[1]);
     }
 
+  
     return_value = chdir(new_dir);
     if (return_value == -1) {
         perror("cd");
-        if (tokens[1][0] != '/') {
-            free(new_dir);
-        }
         return -1;
     }
 
     if (getcwd(cwd, sizeof(cwd)) == NULL) {
         perror("getcwd");
-        if (tokens[1][0] != '/') {
-            free(new_dir);
-        }
         return -1;
     }
-    
     if (setenv("PWD", cwd, 1) == -1) {
         perror("setenv");
-        if (tokens[1][0] != '/') {
-            free(new_dir);
-        }
         return -1;
     }
-    
     if (setenv("OLDPWD", prev_dir != NULL ? prev_dir : "", 1) == -1) {
         perror("setenv");
-        if (tokens[1][0] != '/') {
-            free(new_dir);
-        }
         return -1;
-    }
-
-    if (tokens[1][0] != '/') {
-        free(new_dir);
     }
 
     return 0;
