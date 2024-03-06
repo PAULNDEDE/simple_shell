2f520f71f6e3aacbf1e3c04182c1963146d99152
Kan215 <katamialvin215@gmail.com>
2024-03-06 15:59:59 +0300

    cd

--- cd.c
+++ cd.c
@@ -9,41 +9,48 @@
  * @args: user input arguments
  * Return: Always 0 (success), else -1 on error
  */
-int cd(char **tokens) {
-    char *home_dir = getenv("HOME");
-    char *prev_dir = getenv("PWD");
-    int return_value = 0;
+	int cd(char **tokens)
+	{
+	char *home_dir = getenv("HOME");
+	char *prev_dir = getenv("PWD");
+	int return_value = 0;
 
-    
-    char cwd[1024];
+	char cwd[1024];
 
-    if (!tokens[1] || strcmp(tokens[1], "~") == 0) {
-        return_value = chdir(home_dir);
-    } else if (strcmp(tokens[1], "-") == 0) {
-        if (!prev_dir) {
-            fprintf(stderr, "cd: OLDPWD not set\n");
-            return -1;
-        }
-        return_value = chdir(prev_dir);
-    } else {
-        return_value = chdir(tokens[1]);
-    }
+	if (!tokens[1] || strcmp(tokens[1], "~") == 0)
+	{
+	(return_value = chdir(home_dir));
+	}
+	else if (strcmp(tokens[1], "-") == 0)
+	{
+	if (!prev_dir)
+	{
+	fprintf(stderr, "cd: OLDPWD not set\n");
+	return (-1);
+	}
+	(return_value = chdir(prev_dir));
+	}
+	else
+	{
+	(return_value = chdir(tokens[1]));
+	}
 
-    if (return_value == -1) {
-        perror("cd");
-        return -1;
-    }
+	if (return_value == -1)
+	{
+	perror("cd");
+	return (-1);
+	}
 
-    
-    if (getcwd(cwd, sizeof(cwd)) != NULL) {
-        setenv("PWD", cwd, 1);
-    } else {
-        perror("getcwd() error");
-        return -1;
-    }
 
-    /* Update OLDPWD environment variable*/
-    setenv("OLDPWD", prev_dir ? prev_dir : "", 1);
-
-    return 0;
+	if (getcwd(cwd, sizeof(cwd)) != NULL)
+	{
+	setenv("PWD", cwd, 1);
+	}
+	else
+	{
+	perror("getcwd() error");
+	return (-1);
+	}
+	setenv("OLDPWD", prev_dir ? prev_dir : "", 1);
+	return (0);
 }
